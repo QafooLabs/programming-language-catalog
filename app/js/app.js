1e46b44dc4751a374abe9d4d46f6f48ddf0000b0
Jakob Westhoff <jakob@westhoffswelt.de>
2012-08-02 17:09:25 +0200

    Added empty detail page and configured app

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,10 @@
 'use strict';
 
-
-// Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
-    $routeProvider.otherwise({redirectTo: '/view1'});
-  }]);
+angular.module('languageCatalog', []).
+    config(
+        ['$routeProvider', function($routeProvider) {
+            $routeProvider.when('/', {templateUrl: 'partials/landing.html', controller: LandingController});
+            $routeProvider.when('/:language', {templateUrl: 'partials/language.html', controller: LanguageController});
+            $routeProvider.otherwise({redirectTo: '/'});
+        }]
+    );
---
+++ app/js/controllers/language.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function LanguageController($scope, $http) {
+
+};
+
+// DI injection definition (Only needed if code is minified)
+LanguageController.$inject = ['$scope', '$http'];
