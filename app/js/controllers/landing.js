0c7749147d65c29d77165cba701950861c3bdc7a
Jakob Westhoff <jakob@westhoffswelt.de>
2012-08-02 17:08:44 +0200

    Added landing page controller

---
+++ app/js/controllers/landing.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * Controller to be used for the initial landing page
+ *
+ * @param $scope
+ * @constructor
+ */
+function LandingController($scope) {
+}
+
+// DI injection definition (Only needed if code is minified)
+LandingController.$inject = [];
